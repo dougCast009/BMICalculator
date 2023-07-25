1f5c2b2c8087b7480fd4e70f5093cab1676643d8
dougCast009 <douglascastillo087@gmail.com>
2023-07-24 19:22:51 -0600

    Estudio 24/7/2023

--- app/src/main/java/com/dcastillo/bmicalculator/MainActivity.java
+++ app/src/main/java/com/dcastillo/bmicalculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.dcastillo.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String alertText = "This is my variable text";
-        Toast.makeText(this, alertText, Toast.LENGTH_LONG).show();
+
+        TextView resultText = findViewById(R.id.text_view_result);
+
+        resultText.setText("Wohoo, I can update my textview using programming code - I'm a real developer!");
+
+        RadioButton maleButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleButton = findViewById(R.id.radi_button_female);
+
+        EditText ageEditText = findViewById(R.id.edit_text_age);
+        EditText heightEditText = findViewById(R.id.edit_text_height);
+        EditText weightEditText = findViewById(R.id.edit_text_weight);
+
+        Button calculateButton = findViewById(R.id.button_calculate);
     }
 }
