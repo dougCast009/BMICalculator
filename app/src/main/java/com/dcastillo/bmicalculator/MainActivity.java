335056417a5a29e8797d7de453c8c4a5e0417e35
dougCast009 <douglascastillo087@gmail.com>
2023-08-04 05:27:35 -0600

    Estudio 3/8/2023

--- app/src/main/java/com/dcastillo/bmicalculator/MainActivity.java
+++ app/src/main/java/com/dcastillo/bmicalculator/MainActivity.java
@@ -12,35 +12,59 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //Class Variables, also called fields
-    TextView resultText;
-    Button calculateButton;
+    private TextView resultText;
+    private Button calculateButton;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private EditText ageEditText;
+    private EditText heightEditText;
+    private EditText weightEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
-
-        calculateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Wow! We can react to button click", Toast.LENGTH_LONG).show();
-            }
-        });
+        setupButtonClickListener();
     }
 
     private void findViews(){
         resultText = findViewById(R.id.text_view_result);
-
         resultText.setText("Wohoo, I can update my textview using programming code - I'm a real developer!");
-
-        RadioButton maleButton = findViewById(R.id.radio_button_male);
-        RadioButton femaleButton = findViewById(R.id.radi_button_female);
-
-        EditText ageEditText = findViewById(R.id.edit_text_age);
-        EditText heightEditText = findViewById(R.id.edit_text_height);
-        EditText weightEditText = findViewById(R.id.edit_text_weight);
-
+        maleButton = findViewById(R.id.radio_button_male);
+        femaleButton = findViewById(R.id.radi_button_female);
+        ageEditText = findViewById(R.id.edit_text_age);
+        heightEditText = findViewById(R.id.edit_text_height);
+        weightEditText = findViewById(R.id.edit_text_weight);
         calculateButton = findViewById(R.id.button_calculate);
+    }
+
+    private void setupButtonClickListener() {
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateBmi();
+            }
+        });
+    }
+
+    private void calculateBmi() {
+        String ageText = ageEditText.getText().toString();
+        String heightText = heightEditText.getText().toString();
+        String weightText = weightEditText.getText().toString();
+
+        //Converting the the strings to int
+        int age = Integer.parseInt(ageText);
+        double height = Double.parseDouble(heightText);
+        int weight = Integer.parseInt(weightText);
+
+        double heightInMeters = height / 100;
+
+        //Formula
+        double bmi = weight/(heightInMeters*heightInMeters);
+
+        //We must convert the decimal to string
+        String bmiTextResult = String.valueOf(bmi);
+        resultText.setText(bmiTextResult);
     }
 }
